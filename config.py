f1e927cbffbb55017b6e40e1b4815af942c34d24
maxmax <maxbakaiev@gmail.com>
2023-08-24 23:41:19 +0300

    local

--- config.py
+++ config.py
@@ -12,5 +12,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-ex-email@example.com']
-    POSTS_PER_PAGE = 3
     LANGUAGES = ['en', 'ua']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    POSTS_PER_PAGE = 5
